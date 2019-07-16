034d6d23838ab83586a286dfe239b3ef7adc3716
Tanguy Jouvin <42809139+tanguyJouvin@users.noreply.github.com>
2019-07-16 13:34:21 +0200

    Update Cart.js

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,11 +2,14 @@ import React from "react";
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-// import { countTotalPrice } from '../../Utils/countTotalPrice';
+import  countTotalPrice from '../../Utils/countTotalPrice';
 import { removeArticle } from '../../Actions/cartAction';
 
 
 function Cart(props) {
+
+
+
   return (
     <div className="col-md-4">
       <div className="card">
@@ -20,7 +23,12 @@ function Cart(props) {
                     <th className="px-2" scope="row" width="50%">{article.name}</th>
                     <td width="30%">{article.price} $</td>
                     <td width="20%">
-                      <button className="btn btn-warning btn-sm float-right mr-2" onClick={() => props.dispatch(removeArticle(index))}>remove</button>
+                    <button
+                      className="btn btn-warning btn-sm float-right mr-2"
+                      onClick={() => props.dispatch(removeArticle(index))}//enlève l'index du produit du tableau (= panier ' Cart') si on avait prit name on aurait pu supprimer plusieurs occurence du produit
+                    >
+                      remove
+                    </button>
                     </td>
                   </tr>
                 )
@@ -29,16 +37,16 @@ function Cart(props) {
           </table>
         </div>
         <div className="card-footer">
-          {/* <h3>Total price : <span className="float-right">{countTotalPrice(props.articles)} $</span></h3> */}
+          <h3>Total price : <span className="float-right">{countTotalPrice(props.articles)} $</span></h3> {/*on passe tous les articles pour les additionner*/}
         </div>
       </div>
-      {/* {props.articles.length === 0 ? null : <Link to="/checkout"><button className="btn btn-success mt-3">Checkout</button></Link>} */}
+      {props.articles.length === 0 ? null : <Link to="/checkout"><button className="btn btn-success mt-3">Checkout</button></Link>}
     </div>
   )
 }
 
-const mapStatetoProps = store => ({
-  articles: store
+const mapStateToProps = store => ({
+  articles: store ,//retourne un objet qui stocke le contenu du panier (=donc tout le store) dans la propriété articles,
 });
 
-export default connect(mapStatetoProps)(Cart);
+export default connect(mapStateToProps)(Cart);//mapsStateToProps = importe le store dans les props du composant
